8f7ede1800174f40e6fa47a0020fcc2d8b9433f4
iaheung <tur20579@temple.edu>
2023-02-03 19:02:03 +0900

    Create Repository

--- app/src/main/java/edu/temple/assignment1_helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/assignment1_helloworld/MainActivity.kt
@@ -2,10 +2,14 @@ package edu.temple.assignment1_helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textview)
+        findViewById<View>(R.id.button).setOnClickListener{textView.text = "@string/button_clicked_text"}
     }
 }
